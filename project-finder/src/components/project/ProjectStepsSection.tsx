0bf4ec92fa156ec8b225b0b3b3e84905c1595590
linacqa <linacqaa@gmail.com>
2026-04-29 14:43:33 +0300

    fix

--- project-finder/src/components/project/ProjectStepsSection.tsx
+++ project-finder/src/components/project/ProjectStepsSection.tsx
@@ -67,28 +67,26 @@ export default function ProjectStepsSection({
 									)}
 									as="div"
 								/>
-								{accountInfo?.role !== "admin" && (
-									<TTNewButton
-										variant="secondary"
-										icon={
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												width="14"
-												height="14"
-												viewBox="0 0 16 16"
-												fill="currentColor"
-												aria-hidden="true"
-											>
-												<path d="M12.854.146a.5.5 0 0 0-.708 0L10.5 1.793l3.707 3.707 1.647-1.646a.5.5 0 0 0 0-.708l-3-3z" />
-												<path d="M10.793 2.5 2 11.293V14h2.707L13.5 5.207 10.793 2.5z" />
-											</svg>
-										}
-										size="sm"
-										onClick={() =>
-											setEditingStepId(projectStep.id)
-										}
-									/>
-								)}
+								<TTNewButton
+									variant="secondary"
+									icon={
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											width="14"
+											height="14"
+											viewBox="0 0 16 16"
+											fill="currentColor"
+											aria-hidden="true"
+										>
+											<path d="M12.854.146a.5.5 0 0 0-.708 0L10.5 1.793l3.707 3.707 1.647-1.646a.5.5 0 0 0 0-.708l-3-3z" />
+											<path d="M10.793 2.5 2 11.293V14h2.707L13.5 5.207 10.793 2.5z" />
+										</svg>
+									}
+									size="sm"
+									onClick={() =>
+										setEditingStepId(projectStep.id)
+									}
+								/>
 							</div>
 							{editingStepId === projectStep.id && (
 								<div className="mt-2">
